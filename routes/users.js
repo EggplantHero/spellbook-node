1772eba73d08c44d5cb00bbfef33d664f96814c4
EggplantHero <jessetristanweber@gmail.com>
2020-08-19 06:10:09 -0400

    fixed case sensitivity for email login

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const auth = require("../middleware/auth");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const express = require("express");
+const { isError } = require("lodash");
 const router = express.Router();
 
 router.get("/me", auth, async (req, res) => {
@@ -16,11 +17,12 @@ router.post("/", async (req, res) => {
     return res.status(400).send(error.details[0].message);
   }
 
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ email: req.body.email.toLowerCase() });
   if (user) return res.status(400).send("User already registered.");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
+  user.email = user.email.toLowerCase();
   user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
